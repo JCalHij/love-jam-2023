b3c7c62534e37054ba3cb00944cecf12b7c78ee9
JCalHij <38001598+JCalHij@users.noreply.github.com>
2023-02-20 11:05:13 +0100

    LostScreenEffect

--- game/gameplay/effects.lua
+++ game/gameplay/effects.lua
@@ -31,3 +31,33 @@ function TrackerEffect:destroy()
     self.unit = nil
     self.alive = false
 end
+
+
+
+---@class LostScreenEffect: Effect
+---@operator call(): LostScreenEffect
+LostScreenEffect = Effect:extend()
+
+
+function LostScreenEffect:new()
+    self.alive = true
+    self.alpha = 0.0
+    self.fade_in_speed = 0.3
+end
+
+function LostScreenEffect:update(dt)
+    self.alpha = math.min(self.alpha + self.fade_in_speed*dt, 1.0)
+end
+
+function LostScreenEffect:render()
+    SetDrawColor({0, 0, 0, self.alpha})
+    DrawRectangle({x=0, y=0, w=VirtualWidth, h=VirtualHeight})
+    SetDrawColor({1, 1, 1, self.alpha})
+    love.graphics.printf("YOU LOST", 0, VirtualHeight/2, VirtualWidth, "center")
+    SetDrawColor({1, 1, 1, 1})
+end
+
+function LostScreenEffect:destroy()
+    self.unit = nil
+    self.alive = false
+end
