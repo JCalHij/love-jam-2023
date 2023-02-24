22e0ba3ae4e3dd8af15bbccf4514746bdc6cefb4
JCalHij <38001598+JCalHij@users.noreply.github.com>
2023-02-24 11:45:16 +0100

    math.round

--- core/math_ex.lua
+++ core/math_ex.lua
@@ -8,3 +8,14 @@ Returns the value `value` clamped between range [`min`, `max`].
 function math.clamp(value, min, max)
     return math.max(min, math.min(value, max))
 end
+
+--[[
+Returns the rounded value for `value`.
+
+TODO[javi]: Maybe this in the future? https://stackoverflow.com/questions/18313171/lua-rounding-numbers-and-then-truncate
+]]
+---@param value number
+---@return integer
+function math.round(value)
+    return math.floor(value + 0.5)
+end
